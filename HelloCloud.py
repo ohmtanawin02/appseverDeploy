28dd12f5f6edcc5e40c7b891b1e67dc08e8b82f5
INE-01 <INE-01@Admins-iMac-6.local>
2020-07-29 18:09:38 +0700

    edit

--- HelloCloud.py
+++ HelloCloud.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Hello RUK-COM is Great !</h1>'
+    return '<h1>Hello RUK-COM is Great KUB!</h1>'
 
 @app.route('/home', methods=['GET', 'POST'])
 def home():
